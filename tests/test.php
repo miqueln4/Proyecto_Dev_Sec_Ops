14bbf1f9c2670ab19fe77faf8145783f95f8c824
miqueln4 <miquel.navarro.ciber@iescamp.es>
2024-05-24 12:38:09 +0200

    prueba 2

--- tests/test.php
+++ tests/test.php
@@ -6,41 +6,38 @@ include('../public/php/functions.php');
 
 class test extends TestCase
 {
-    private $validator;
-
-    protected function setUp(): void
-    {
-        $this->validator = new Validator();
-    }
-
     public function testCheckDniValid()
     {
-        $this->assertTrue($this->validator->check_dni("12345678Z"));
-        $this->assertTrue($this->validator->check_dni("87654321A"));
+        $function = new Functions();
+        $this->assertTrue($function->check_dni("12345678Z"));
+        $this->assertTrue($function->check_dni("87654321A"));
     }
 
     public function testCheckDniInvalid()
     {
-        $this->assertFalse($this->validator->check_dni("12345678A")); // Letra no valida
-        $this->assertFalse($this->validator->check_dni("A2345678Z")); // Formato no valido
-        $this->assertFalse($this->validator->check_dni("1234567Z"));  // Muy corta
-        $this->assertFalse($this->validator->check_dni("123456789Z")); // Muy larga
-        $this->assertFalse($this->validator->check_dni("")); // Vacio
+        $function = new Functions();
+        $this->assertFalse($function->check_dni("12345678A")); // Letra no valida
+        $this->assertFalse($function->check_dni("A2345678Z")); // Formato no valido
+        $this->assertFalse($function->check_dni("1234567Z"));  // Muy corta
+        $this->assertFalse($function->check_dni("123456789Z")); // Muy larga
+        $this->assertFalse($function->check_dni("")); // Vacio
     }
 
     public function testValidatePasswordValid()
     {
-        $this->assertTrue($this->validator->validatePassword("Valid123!"));
-        $this->assertTrue($this->validator->validatePassword("Anoth3r$"));
+        $function = new Functions();
+        $this->assertTrue($function->validatePassword("Valid123!"));
+        $this->assertTrue($function->validatePassword("Anoth3r$"));
     }
 
     public function testValidatePasswordInvalid()
     {
-        $this->assertFalse($this->validator->validatePassword("short1A!")); // Muy corta
-        $this->assertFalse($this->validator->validatePassword("alllowercase1!")); // Sin mayuscula
-        $this->assertFalse($this->validator->validatePassword("ALLUPPERCASE1!")); // Sin minuscula
-        $this->assertFalse($this->validator->validatePassword("NoNumber!")); // Sin numero
-        $this->assertFalse($this->validator->validatePassword("NoSpecial1")); // Sin caracter especial
-        $this->assertFalse($this->validator->validatePassword("")); // Vacia
+        $function = new Functions();
+        $this->assertFalse($function->validatePassword("short1A!")); // Muy corta
+        $this->assertFalse($function->validatePassword("alllowercase1!")); // Sin mayuscula
+        $this->assertFalse($function->validatePassword("ALLUPPERCASE1!")); // Sin minuscula
+        $this->assertFalse($function->validatePassword("NoNumber!")); // Sin numero
+        $this->assertFalse($function->validatePassword("NoSpecial1")); // Sin caracter especial
+        $this->assertFalse($function->validatePassword("")); // Vacia
     }
 }
